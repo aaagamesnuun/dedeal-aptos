5aa508dc41c58bb7f253e275ba1b8891794f86fa
aaagamesnuun <aaagamesnuun@gmail.com>
2023-04-15 21:11:40 +0900

    13:Implement deal-related actions and state management

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -1,12 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import { Eye4eyeContext } from '../context/Eye4eyeContext';
 
 const Deal = () => {
   const params = useParams();
-  const { eye4eyeGetDeal } = useContext(Eye4eyeContext);
+
+  const {
+    eye4eyeGrant,
+    eye4eyeBuyerExecuteSeller,
+    eye4eyeSellerExecuteBuyer,
+    eye4eyeReleaseDeposit,
+    eye4eyeGetDeal,
+  } = useContext(Eye4eyeContext);
 
   const [deal, setDeal] = useState(null);
+
+  const handleGrant = () => {
+    eye4eyeGrant(params.dealId);
+  };
+  const handleBuyerExecuteSeller = () => {
+    eye4eyeBuyerExecuteSeller(params.dealId);
+  };
+  const handleSellerExecuteBuyer = () => {
+    eye4eyeSellerExecuteBuyer(params.dealId);
+  };
+  const handleReleaseDeposit = () => {
+    eye4eyeReleaseDeposit(params.dealId);
+  };
 
   useEffect(() => {
   }, []);
