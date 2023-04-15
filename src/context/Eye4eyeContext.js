3098523c49a3c0d71d30d45abbd025deac8f5eb1
aaagamesnuun <aaagamesnuun@gmail.com>
2023-04-15 21:22:22 +0900

    15:create Eye4eyeContext.js,conect.js,Eye4eyes.json

---
+++ src/context/Eye4eyeContext.js
@@ -0,0 +1,8 @@
+import { createContext, useEffect } from "react";
+import React, { useState } from "react";
+export const Eye4eyeContext = createContext();
+
+const { ethereum } = window;
+
+export const Eye4eyeProvider = ({ children }) => {
+};
