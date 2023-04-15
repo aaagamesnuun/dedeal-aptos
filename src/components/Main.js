0afb0bc50b84c9ddc1d714164129554266748927
aaagamesnuun <aaagamesnuun@gmail.com>
2023-04-15 20:30:05 +0900

    6: input fields and buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,14 +7,38 @@ const Main = () => {
   const navigate = useNavigate();
   const { eye4eyeClaim, handleChange, inputFormData } = useContext(Eye4eyeContext);
 
+
+
   return (
     <div className='mainContainer'>
       <div className="inputContainer">
         <h1 className='title'>Seller</h1>
 
+        <input
+          type="number"
+          placeholder='Payment Amount'
+          name="_amountOfClaim"
+          step="0.0001"
+          onChange={(e) => handleChange(e, "_amountOfClaim")}
+        />
+
+        <button type='button'>
+          Make a New Deal
+        </button>
+
       </div>
 
       <div className="inputContainer">
+        <input
+          type="number"
+          placeholder='Deal ID'
+          name="dealId"
+          step="1"
+          onChange={(e) => handleChange(e, "dealId")}
+        />
+        <button type='button'>
+          Find an Existing Deal
+        </button>
       </div>
     </div>
   );
