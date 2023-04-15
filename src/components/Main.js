1b930b49a6ef8d80a65a3a647c720100ebd96e1d
aaagamesnuun <aaagamesnuun@gmail.com>
2023-04-15 20:32:22 +0900

    7:event handler functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,25 @@ const Main = () => {
   const navigate = useNavigate();
   const { eye4eyeClaim, handleChange, inputFormData } = useContext(Eye4eyeContext);
 
+  const handleSubmit = () => {
+    const { _amountOfClaim, _grantDeadline, _executeDeadlineInterval } = inputFormData;
+    console.log("_amountOfClaim : %s", _amountOfClaim);
+    console.log("_grantDeadline : %s", _grantDeadline);
+    console.log("_executeDeadlineInterval : %s", _executeDeadlineInterval);
 
+    if (_amountOfClaim === "" || _grantDeadline === "" || _executeDeadlineInterval === "") {
+      console.log("There are empty input fields.");
+      return;
+    } else {
+      eye4eyeClaim();
+    }
+  };
+  
+
+  const handleFindDeal = () => {
+    const { dealId } = inputFormData;
+    navigate(`/deal/${dealId}`);
+  };
 
   return (
     <div className='mainContainer'>
@@ -22,7 +40,28 @@ const Main = () => {
           onChange={(e) => handleChange(e, "_amountOfClaim")}
         />
 
-        <button type='button'>
+        <p>The price of the item that the seller and buyer add to the seller deposit.</p>
+
+        <input
+          type="number"
+          placeholder='Deadline for buyer to pay(hour)'
+          name="_grantDeadline"
+          onChange={(e) => handleChange(e, "_grantDeadline", true)}
+        />
+
+        <p>The deadline for the buyer to pay the seller deposit.</p>
+
+        <input
+          type="number"
+          placeholder='Time Added to Deposit Release Time Per Action(hour)'
+          name="_executeDeadlineInterval"
+          step="1"
+          onChange={(e) => handleChange(e, "_executeDeadlineInterval", true)}
+        />
+
+        <p>Time to deadline that extends each time the seller and buyer execute or grant.</p>
+
+        <button type='button' onClick={handleSubmit}>
           Make a New Deal
         </button>
 
@@ -36,7 +75,7 @@ const Main = () => {
           step="1"
           onChange={(e) => handleChange(e, "dealId")}
         />
-        <button type='button'>
+        <button type='button' onClick={handleFindDeal}>
           Find an Existing Deal
         </button>
       </div>
